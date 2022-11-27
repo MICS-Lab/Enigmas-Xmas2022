e68cf0118b5d6feb6c1bf0df6bab1b51c62276b5
pauldubois98 <pauldubois98@gmail.com>
2022-11-27 13:43:02 +0100

    added description

--- build.py
+++ build.py
@@ -18,7 +18,10 @@ middle = ""
 for i, enigma in enumerate(data['enigmas']):
     middle += f"""
         <div class="enigma" id="enigma{i}">
-            <h4>Enigma {i+1}</h4>
+            <div class="title">
+                <h4>Enigma {i+1}:</h4>
+                <p>{enigma['description']}</p>
+            </div>
             <div class="input">
                 <label for="password{i}">Answer:</label>
                 <input type="password{i}" id="password{i}" name="password{i}" />
