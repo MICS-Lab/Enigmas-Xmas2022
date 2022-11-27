4bedeea1313a100e52c24f311093597df180adc0
pauldubois98 <pauldubois98@gmail.com>
2022-11-27 11:27:47 +0100

    python read yaml data file

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 import hashlib
+import yaml
 
-m = hashlib.sha256()
-m.update("blah".encode('utf8'))
-m.digest()
-print(m.hexdigest())
+
+# Read YAML file
+with open("data.yaml", 'r') as stream:
+    data = yaml.safe_load(stream)
+
+for enigma in data['enigmas']:
+    print(enigma)
+    m = hashlib.sha256()
+    m.update(enigma['answer'].encode('utf8'))
+    m.digest()
+    print(m.hexdigest())
